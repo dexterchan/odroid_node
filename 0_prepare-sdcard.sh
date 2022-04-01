58e476c97d0224c0e1bf0eb8c7c49564b691a719
dexterchan <dexterchan@gmail.com>
2022-04-02 00:03:28 +0800

    add vpn client

--- 0_prepare-sdcard.sh
+++ 0_prepare-sdcard.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 cp ~/.ssh/wpa_supplicant.conf /Volumes/boot
 cp ~/.ssh/postgredb.connection_string.sh /Volumes/boot
+cp ~/.ssh/surfshark.credential  /Volumes/boot
 #echo cgroup_memory=1 cgroup_enable=memory $(cat /Volumes/boot/cmdline.txt ) > /Volumes/boot/cmdline.txt
 cp 1_soup-base.setup_no_docker.sh /Volumes/boot
 cp install-docker.sh /Volumes/boot
 cp install-k3s-server.sh /Volumes/boot
 cp install-postgre.sh /Volumes/boot
-cp install-nfs.sh /Volumes/boot
+cp install-nfs-server.sh /Volumes/boot
