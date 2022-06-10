2f1b3425e3e9f1da8a1f863f9ded8ab310530c92
dexterchan <dexterchan@gmail.com>
2022-06-10 22:20:45 +0800

    fix dns issue of m1

--- m1_1_soup-base.setup_no_docker.sh
+++ m1_1_soup-base.setup_no_docker.sh
@@ -32,7 +32,7 @@ sudo apt update -y
 sudo apt full-upgrade -y
 #As of APR2022
 #Odroid Ubuntu failed to have complete iptable to support ufw
-sudo apt install -y  avahi-daemon curl unzip nfs-common cifs-utils telnet dnsutils grub2-common
+sudo apt install -y  avahi-daemon curl unzip nfs-common cifs-utils telnet dnsutils grub2-common resolvconf
 
 cd /tmp
 curl -fsSL https://get.docker.com -o get-docker.sh
@@ -44,33 +44,34 @@ sudo usermod -a -G sudo $APPUSER
 sudo sed -i 's/#host-name=foo/host-name='${host_name}'/g' /etc/avahi/avahi-daemon.conf
 sudo chown -R ${APPUSER}:${APPUSER} $HOMEDIR
 
+#Fix the DNS issue
+sudo dpkg-reconfigure resolvconf
+
+
+# #sudo apt-get install ufw -y
+# #Disable ipv6 as the current version iptableV6 not working 
+# sudo sed -i 's/IPV6=yes/IPV6=no/g' /etc/default/ufw
 
 
 
-#sudo apt-get install ufw -y
-#Disable ipv6 as the current version iptableV6 not working 
-sudo sed -i 's/IPV6=yes/IPV6=no/g' /etc/default/ufw
+# # Known issue of nftables, need to revert back to iptables-legacy
+# sudo update-alternatives --set iptables /usr/sbin/iptables-legacy
+# sudo update-alternatives --set ip6tables /usr/sbin/ip6tables-legacy
+# sudo update-alternatives --set arptables /usr/sbin/arptables-legacy
+# sudo update-alternatives --set ebtables /usr/sbin/ebtables-legacy
+
+# sudo ln -s /etc/alternatives/iptables /sbin/iptables
+# sudo ln -s /etc/alternatives/iptables-save /sbin/iptables-save
+# sudo ln -s /etc/alternatives/iptables-restore /sbin/iptables-restore
+# sudo ln -s /etc/alternatives/ip6tables /sbin/ip6tables
+# sudo ln -s /etc/alternatives/ip6tables-save /sbin/ip6tables-save
+# sudo ln -s /etc/alternatives/ip6tables-restore /sbin/ip6tables-restore
+
+# echo "restart before applying below"
+# echo sudo ufw allow from 192.168.1.0/24 proto tcp to any port 22
+# echo sudo ufw deny from 192.168.1.3 to any
+# echo sudo ufw deny from 192.168.1.17 to any
 
 
-
-# Known issue of nftables, need to revert back to iptables-legacy
-sudo update-alternatives --set iptables /usr/sbin/iptables-legacy
-sudo update-alternatives --set ip6tables /usr/sbin/ip6tables-legacy
-sudo update-alternatives --set arptables /usr/sbin/arptables-legacy
-sudo update-alternatives --set ebtables /usr/sbin/ebtables-legacy
-
-sudo ln -s /etc/alternatives/iptables /sbin/iptables
-sudo ln -s /etc/alternatives/iptables-save /sbin/iptables-save
-sudo ln -s /etc/alternatives/iptables-restore /sbin/iptables-restore
-sudo ln -s /etc/alternatives/ip6tables /sbin/ip6tables
-sudo ln -s /etc/alternatives/ip6tables-save /sbin/ip6tables-save
-sudo ln -s /etc/alternatives/ip6tables-restore /sbin/ip6tables-restore
-
-echo "restart before applying below"
-echo sudo ufw allow from 192.168.1.0/24 proto tcp to any port 22
-echo sudo ufw deny from 192.168.1.3 to any
-echo sudo ufw deny from 192.168.1.17 to any
-
-
-echo sudo ufw --force default deny incoming
-echo sudo ufw --force enable 
+# echo sudo ufw --force default deny incoming
+# echo sudo ufw --force enable 
